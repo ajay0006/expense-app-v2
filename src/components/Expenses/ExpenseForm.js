d12e88775ff6fb09dc3178a453171152c60ad415
damilola <ajay0006@algonquinlive.com>
2023-10-17 20:29:04 -0400

    passing data from child to parent

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,7 +1,7 @@
 import './ExpenseForm.css'
 import {useState} from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [title, setTitle] = useState("")
     const [amount, setAmount] = useState("")
     const [date, setDate] = useState("")
@@ -17,20 +17,38 @@ const ExpenseForm = () => {
        setDate(e.target.value)
     }
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        const expenseData = {
+            title,
+            amount,
+            date: new Date(date)
+        }
+        // so now i am passing the expense data from the form to the function that was bound to the props that was passed down to the child component aka ExpenseForm
+        // from the parent component NewExpense, this will result in the function being run from the NewExpense component aka parent component, therefore the data will be
+        // accessible above this component
+
+        props.onSaveExpenseData(expenseData)
+        setTitle('')
+        setAmount('')
+        setDate('')
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label> Title</label>
-                    <input type='text' onChange={titleChangeHandler}/>
+                    <input type='text' value={title} onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label> Amount </label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
+                    <input type='number' value={amount} min='0.01' step='0.01' onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label> Date</label>
-                    <input type='date' min='2000-01-01' max={Date(Date.now()).toString()} onChange={dateChangeHandler}/>
+                    <input type='date' value={date} min='2000-01-01' max={Date(Date.now()).toString()} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
--- src/components/Expenses/NewExpense.js
+++ src/components/Expenses/NewExpense.js
@@ -1,10 +1,23 @@
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css'
 
-function NewExpense() {
+function NewExpense(props) {
+    // this is the function i am binding to the props onSaveExpenseData for the sake of passing data from child to parent
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+
+        props.onAddExpense(expenseData)
+    }
     return (
         <div className='new-expense'>
-            <ExpenseForm/>
+            {/* here i am passing data upwards, remember i can pass data from parent to child (downwards) using props
+              but in this case to pass data from child to parent i will have to use functions passed as props
+              so i am passing the prop onSaveExpenseData which points to the function in the parent component
+              to the child ExpenseForm */}
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     )
 }
