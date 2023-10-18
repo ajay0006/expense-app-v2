12c954f85fadd2c19d1c6b59fca4af6234210348
damilola <ajay0006@algonquinlive.com>
2023-10-18 17:10:01 -0400

    completed list component and code transfer

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020')
@@ -15,24 +15,11 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     })
-
-    let expensesNoContent = <p>No Expenses Found !!!</p>
-
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {filteredExpenses.length === 0 ? expensesNoContent :
-                    filteredExpenses.map((expense) => (
-                            <ExpenseItem
-                                key={expense.id}
-                                title={expense.title}
-                                amount={expense.amount}
-                                date={expense.date}
-                            />
-                        )
-                    )
-                }
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
